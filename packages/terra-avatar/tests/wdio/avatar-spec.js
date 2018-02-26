40f830c06fdeb3f16c008853c78103ba0c0379a3
Chase Sinclair <chasersinclair@gmail.com>
2018-02-26 16:01:44 -0600

    Update wdio tests

--- packages/terra-avatar/tests/wdio/avatar-spec.js
+++ packages/terra-avatar/tests/wdio/avatar-spec.js
@@ -1,49 +1,74 @@
-/* global before, browser, Terra */
+/* global beforeEach, browser, Terra */
 
 describe('Avatar', () => {
-  describe('Default', () => {
-    beforeEach(() => browser.url('/#/tests/avatar-tests/default'));
+  describe('User', () => {
+    beforeEach(() => browser.url('/#/tests/avatar-tests/user'));
 
-    Terra.should.beAccessible({ selector: '#default-avatar' });
-    Terra.should.matchScreenshot({ selector: '#default-avatar' });
+    Terra.should.beAccessible({ selector: '#user-avatar' });
+    Terra.should.matchScreenshot({ selector: '#user-avatar' });
     Terra.should.themeEachCustomProperty({
-      '--terra-avatar-background-color': 'rgba((85, 26, 139, 0.1)',
-      '--terra-avatar-border': '0.07143rem solid rgb(85, 26, 139)',
-      '--terra-avatar-box-shadow': 'inset 0 0.07143rem 0.2143rem 0 rgba(85, 26, 139, 0.3)',
+      '--terra-avatar-background-color': 'rgba(0, 0, 255, 0.5)',
+      '--terra-avatar-border': '0.07143rem solid rgb(0, 0, 255)',
+      '--terra-avatar-box-shadow': 'inset 0 0.07143rem 0.2143rem 5px rgba(255, 0, 0, 0.3)',
       '--terra-avatar-size': '5rem',
     });
 
-    it('Expect default to have avatar class', () => {
-      expect(browser.getAttribute('#default-avatar', 'class')).contains('avatar');
+    it('Expect user avatar to have avatar class', () => {
+      expect(browser.getAttribute('#user-avatar', 'class')).contains('avatar');
     });
   });
 
-  describe('Icon', () => {
-    before(() => browser.url('/#/tests/avatar-tests/icon'));
+  describe('Facility', () => {
+    beforeEach(() => browser.url('/#/tests/avatar-tests/facility'));
 
-    Terra.should.beAccessible({ selector: '#icon-avatar' });
-    Terra.should.matchScreenshot({ selector: '#icon-avatar' });
+    Terra.should.beAccessible({ selector: '#facility-avatar' });
+    Terra.should.matchScreenshot({ selector: '#facility-avatar' });
     Terra.should.themeEachCustomProperty({
-      '--terra-avatar-icon-color': 'rgb(85,26,139)',
+      '--terra-avatar-icon-facility': 'rgb(0, 100, 0)',
+      '--terra-avatar-background-color': 'rgba(0, 0, 255, 0.5)',
+      '--terra-avatar-border': '0.07143rem solid rgb(0, 0, 255)',
+      '--terra-avatar-box-shadow': 'inset 0 0.07143rem 0.2143rem 5px rgba(255, 0, 0, 0.3)',
+      '--terra-avatar-size': '5rem',
+    });
+
+    it('Expect facility avatar to have avatar class', () => {
+      expect(browser.getAttribute('#facility-avatar', 'class')).contains('avatar');
     });
   });
 
   describe('Image', () => {
-    before(() => browser.url('/#/tests/avatar-tests/image'));
+    beforeEach(() => browser.url('/#/tests/avatar-tests/image'));
 
     Terra.should.beAccessible({ selector: '#image-avatar' });
     Terra.should.matchScreenshot({ selector: '#image-avatar' });
+    Terra.should.themeEachCustomProperty({
+      '--terra-avatar-border': '0.07143rem solid rgb(0, 0, 255)',
+      '--terra-avatar-box-shadow': 'inset 0 0.07143rem 0.2143rem 5px rgba(255, 0, 0, 0.3)',
+      '--terra-avatar-size': '5rem',
+    });
+
+    it('Expect image avatar to have image class', () => {
+      expect(browser.getAttribute('#image-avatar', 'class')).contains('avatar');
+    });
   });
 
   describe('Initials', () => {
-    before(() => browser.url('/#/tests/avatar-tests/initials'));
+    beforeEach(() => browser.url('/#/tests/avatar-tests/initials'));
 
     Terra.should.beAccessible({ selector: '#initials-avatar' });
     Terra.should.matchScreenshot({ selector: '#initials-avatar' });
     Terra.should.themeEachCustomProperty({
-      '--terra-avatar-font-color': 'rgb(85,26,139)',
+      '--terra-avatar-background-color': 'rgba(0, 0, 255, 0.5)',
+      '--terra-avatar-border': '0.07143rem solid rgb(0, 0, 255)',
+      '--terra-avatar-box-shadow': 'inset 0 0.07143rem 0.2143rem 5px rgba(255, 0, 0, 0.3)',
+      '--terra-avatar-color': 'rgb(0, 0, 255)',
       '--terra-avatar-font-size': '2rem',
       '--terra-avatar-line-height': '5rem',
+      '--terra-avatar-size': '5rem',
+    });
+
+    it('Expect initials avatar to have avatar class', () => {
+      expect(browser.getAttribute('#initials-avatar', 'class')).contains('avatar');
     });
   });
 });
